9106cae56c96a079be5e8399c01b571e9a430ea8
Relintai <relintai@gmail.com>
2019-05-31 18:26:36 +0200

    Added a .gitignore, and the basic module setup stuff.

---
+++ register_types.cpp
@@ -0,0 +1,10 @@
+#include "register_types.h"
+
+void register_voxelman_types() {
+
+}	
+
+void unregister_voxelman_types() {
+
+}
+
---
+++ register_types.h
@@ -0,0 +1,7 @@
+#ifndef VOXELMAN_REGISTER_TYPES_H
+#define VOXELMAN_REGISTER_TYPES_H
+
+void register_voxelman_types();
+void unregister_voxelman_types();
+
+#endif
