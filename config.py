ac18664faec2d84be7c154dec0a271b7cd63fb99
Relintai <relintai@gmail.com>
2019-12-25 14:08:20 +0100

    Doc setup.

--- config.py
+++ config.py
@@ -8,4 +8,48 @@ def configure(env):
 	pass
 
 
+def get_doc_classes():
+    return [
+        "WorldArea",
+
+        "GroundClutterFoliage",
+        "GroundClutter",
+
+        "VoxelmanQueue",
+        "VoxelmanUnboundedQueue",
+        "VoxelLight",
+
+        "VoxelmanLevelGenerator",
+
+        "VoxelSurfaceMerger",
+        "VoxelSurfaceSimple",
+        "VoxelSurface",
+        "VoxelmanLibraryMerger",
+        "VoxelmanLibrarySimple",
+        "VoxelmanLibrary",
+
+        "VoxelCubePoints",
+        "VoxelMesherCubic",
+        "TransvoxelCellData",
+        "VoxelMeshData",
+        "VoxelMesherTransvoxel",
+        "VoxelMesher",
+
+        "PropDataEntity",
+        "PropDataEntry",
+        "PropDataLight",
+        "PropDataMesh",
+        "PropDataProp",
+        "PropDataScene",
+        "PropData",
+
+        "EnvironmentData",
+        "VoxelChunkPropData",
+        "VoxelChunk",
+        "VoxelStructure",
+        "VoxelWorld",
+    ]
+
+def get_doc_path():
+    return "doc_classes"
 
